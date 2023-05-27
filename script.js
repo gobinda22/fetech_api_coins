68da626c872f13a1a490c51d237819611260ab8c
Gobinda Malakar <gobindamalakar68@gmail.com>
2023-05-28 00:23:21 +0530

    script.js

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let coinsData = [];
 
+// fetch api using .then
 fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false')
   .then(response => {
     if (!response.ok) {
@@ -24,6 +25,7 @@ fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=mark
     console.error('Error:', error);
   });
 
+// fetch api using async
 async function fetchData() {
     try {
       const response = await fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false');
